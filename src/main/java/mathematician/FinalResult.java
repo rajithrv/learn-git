8d9a453d0e9312c13fa99d4890802cdfba817745
Rajith Ravi <rajithrv@Rajiths-MacBook-Pro.local>
2024-02-20 18:22:18 -0700

    Added Substraction

--- src/main/java/mathematician/FinalResult.java
+++ src/main/java/mathematician/FinalResult.java
@@ -5,6 +5,8 @@ public class FinalResult {
 	public static void main(String[] args) {
 		Addition addition = new Addition();
 		addition.add(2,5);
+		Substraction sub = new Substraction();
+		sub.sub(5, 2);
 		
 
 	}
---
+++ src/main/java/mathematician/Substraction.java
@@ -0,0 +1,16 @@
+package mathematician;
+
+public class Substraction {
+
+	 int a;
+	 int b;
+	 int c;
+	
+	public void sub(int a, int b) {
+		
+		
+		c = a-b;
+		
+		System.out.println(c);
+	}
+}
