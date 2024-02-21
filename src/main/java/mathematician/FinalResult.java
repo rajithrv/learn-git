91d00e73bcd898613460224c8b13cf4db7a65b25
Rajith Ravi <rajithrv@Rajiths-MacBook-Pro.local>
2024-02-20 18:34:07 -0700

    Revertng "Added Substraction"
    This reverts commit 8d9a453d0e9312c13fa99d4890802cdfba817745.

--- src/main/java/mathematician/FinalResult.java
+++ src/main/java/mathematician/FinalResult.java
@@ -5,8 +5,6 @@ public class FinalResult {
 	public static void main(String[] args) {
 		Addition addition = new Addition();
 		addition.add(2,5);
-		Substraction sub = new Substraction();
-		sub.sub(5, 2);
 		
 
 	}
